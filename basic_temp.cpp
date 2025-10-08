1c851d313ee9bf4dddc211c80a34935256fb7f22
Anmol Kothari <anmolkothari22@gmail.com>
2025-10-08 10:23:51 +0530

    added basic_temp.cpp content

---
+++ basic_temp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+
+
+
+
+
+
+
+void solve(){
+
+
+
+}
+
+
+
+int main(){
+    int n=1;
+    cin>>n;
+    while(n--){
+        solve();
+    }
+    return 0;
+}
