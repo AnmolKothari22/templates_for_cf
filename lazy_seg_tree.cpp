56e8939b2f55a6873c39b1fc2f78aa0c755d51fe
Anmol Kothari <anmolkothari22@gmail.com>
2025-11-26 18:45:29 +0530

    added minimum range update seg tree

--- lazy_seg_tree.cpp
+++ lazy_seg_tree.cpp
@@ -84,3 +84,100 @@ struct SegTree {
              + query(2*cur+2, mid+1, r, ql, qr);
     }
 };
+
+
+
+
+//****************minimum range query with range addition update**************** */
+
+
+/*lazy seg tree: range add updates and range MIN queries*/
+struct SegTree {
+    /*  build:
+            SegTree st(n);
+            st.build(v, 1, 0, n-1);
+        query:
+            st.query(1,0,n-1,l,r)
+
+        update:
+            st.update(1, 0,n-1,l2,r2,value)
+        
+            that one at query and update are important and also 1 at 2nd index in  build
+            all things are 0 indexed
+    */
+    int n;
+    vector<long long> tree, lazy; // tree stores mins, lazy stores pending adds
+
+    SegTree(int n) {
+        this->n = n;
+        tree.assign(4*n+2, 0);
+        lazy.assign(4*n+2, 0);
+    }
+
+    void build(vector<long long> &a, int node, int l, int r) {
+        if (l == r) {
+            tree[node] = a[l];
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(a, node<<1, l, mid);
+        build(a, node<<1|1, mid+1, r);
+        tree[node] = min(tree[node<<1], tree[node<<1|1]);
+    }
+
+    void push(int node, int l, int r) {
+        if (lazy[node] != 0) {
+            tree[node] += lazy[node];
+            if (l != r) {
+                lazy[node<<1] += lazy[node];
+                lazy[node<<1|1] += lazy[node];
+            }
+            lazy[node] = 0;
+        }
+    }
+
+    void update(int node, int l, int r, int ql, int qr, long long val) {
+        push(node, l, r);
+        if (r < ql || qr < l) return;
+
+        if (ql <= l && r <= qr) {
+            lazy[node] += val;
+            push(node, l, r);
+            return;
+        }
+
+        int mid = (l + r) >> 1;
+        update(node<<1, l, mid, ql, qr, val);
+        update(node<<1|1, mid+1, r, ql, qr, val);
+        tree[node] = min(tree[node<<1], tree[node<<1|1]);
+    }
+
+    long long query(int node, int l, int r, int ql, int qr) {
+        push(node, l, r);
+        if (r < ql || qr < l) return LLONG_MAX;
+
+        if (ql <= l && r <= qr) return tree[node];
+
+        int mid = (l + r) >> 1;
+        return min(
+            query(node<<1, l, mid, ql, qr),
+            query(node<<1|1, mid+1, r, ql, qr)
+        );
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
