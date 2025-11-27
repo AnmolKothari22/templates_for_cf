a1f5cb80191013c426565ad3f64448aa48face0e
Anmol Kothari <anmolkothari22@gmail.com>
2025-11-27 19:34:07 +0530

    minor changes

--- lazy_seg_tree.cpp
+++ lazy_seg_tree.cpp
@@ -10,26 +10,19 @@ using namespace std;
 /*lazy seg tree: range updates and range queries*/
 struct SegTree {
     //everything is 0 indexed
-    /*
-    building segtree
-    SegTree st(n);
-    st.build(v, 0, 0, n-1);
-    v->vector , n= lenght
+    /*building segtree
+      SegTree st(n);
+      st.build(v, 0, 0, n-1);
+      v->vector , n= lenght  */
+    /* query; st.query(0, 0, n-1,l,r)
+       update: st.update(0, 0, n-1,l,r, value)
     */
-
-    /*
-    query; st.query(0, 0, n-1,l,r)
-    update: st.update(0, 0, n-1,l,r, value)
-    */
-   
     int n;
     vector<long long> seg, lazy;
-
     SegTree(int n) : n(n) {
         seg.assign(4*n, 0);
         lazy.assign(4*n, 0);
     }
-
     // build from array
     void build(vector<long long> &a, int cur, int l, int r) {
         if(l == r){
@@ -41,7 +34,6 @@ struct SegTree {
         build(a, 2*cur+2, mid+1, r);
         seg[cur] = seg[2*cur+1] + seg[2*cur+2];
     }
-
     // push lazy values down
     void push(int cur, int l, int r) {
         if(lazy[cur] != 0){
@@ -53,7 +45,6 @@ struct SegTree {
             lazy[cur] = 0;
         }
     }
-
     // range update: add val to [ql, qr]
     void update(int cur, int l, int r, int ql, int qr, long long val) {
         push(cur, l, r);
@@ -64,14 +55,12 @@ struct SegTree {
             push(cur, l, r);
             return;
         }
-
         int mid = (l+r)/2;
         update(2*cur+1, l, mid, ql, qr, val);
         update(2*cur+2, mid+1, r, ql, qr, val);
 
         seg[cur] = seg[2*cur+1] + seg[2*cur+2];
     }
-
     // range query: sum of [ql, qr]
     long long query(int cur, int l, int r, int ql, int qr) {
         push(cur, l, r);
