26d575df3a1250f775c94fbb558e7445a0e0c000
Anmol Kothari <anmolkothari22@gmail.com>
2026-02-18 00:22:19 +0530

    added template to print any single digit  data structure except variable sized arrays

--- basic_temp.cpp
+++ basic_temp.cpp
@@ -3,26 +3,13 @@
 using namespace std;
 
 
+
 template<typename T>
-T print_v(vector<T>&v){
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-    return 0;
-}
-
-
-void print_mp(map<int,int>&mp){
-    for(auto itr = mp.begin();itr!=mp.end();itr++){
-        cout<<(itr->first)<<"=>>"<<(itr->second)<<"   ";
-    }
-}
-
-void print_set(set<int>&se){
-     for(auto itr = se.begin();itr!=se.end();itr++){
+void print(T itr_start,T itr_end){
+    for(T itr=itr_start;itr!=itr_end;itr++){
         cout<<(*itr)<<" ";
     }
+    cout<<endl;
 }
 
 
